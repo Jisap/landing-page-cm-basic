2a7c64566fe1d4fc3d1fa533006fa51d3ca4b1a6
Jorge Matos <jisapjisap@gmail.com>
2024-06-07 20:21:37 +0200

    work with nav mobile

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
 const navTrigger = document.querySelector("#navTrigger")
 
 // select navigation
-const nav = document.querySelector('#nav')
+const nav = document.querySelector('#nav');
+
+// Select the nav close btn
+const navCloseBtn = document.querySelector("#navCloseBtn");
+
+
+// add a click event listener to the nav trigger btn
+navTrigger.addEventListener("click", () => {
+  // Añade la clase "is-open" que cambia right: 0 -> muestra el nav mobile
+  nav.classList.toggle("is-open")
+})
+
+// add a click event listener to the nav close btn
+navCloseBtn.addEventListener("click", () => {
+  // Borra la clase "is-open" que cambia right: 0 -> cierra el nav mobile
+  nav.classList.remove("is-open")
+})
