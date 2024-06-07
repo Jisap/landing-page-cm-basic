e68720154084c1831266f47264eebc1abac4e55d
Jorge Matos <jisapjisap@gmail.com>
2024-06-07 20:44:48 +0200

    scrollY

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const nav = document.querySelector('#nav');
 // Select the nav close btn
 const navCloseBtn = document.querySelector("#navCloseBtn");
 
+// Select header
+const header = document.querySelector('#header');
+
 
 // add a click event listener to the nav trigger btn
 navTrigger.addEventListener("click", () => {
@@ -18,4 +21,16 @@ navTrigger.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   // Borra la clase "is-open" que cambia right: 0 -> cierra el nav mobile
   nav.classList.remove("is-open")
+})
+
+//add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+  //get the current vertical position
+  const scrollY = window.scrollY;
+
+  //add or remove the active class from the header based on the scroll position
+  scrollY > 50
+    ? header.classList.add("is-active")
+    : header.classList.remove("is-active")
+
 })
